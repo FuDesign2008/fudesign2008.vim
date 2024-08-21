f84a9d87b5b59556299339bffc79b1059db7694d
fuyg <fuyg@lenovocloud.com>
2024-08-21 13:38:52 +0800

    fix: bootstrap.sh - format

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ die() {
 }
 
 lnif() {
-    if [ ! -e "$2" ] ; then
+    if [ ! -e "$2" ]; then
         ln -s "$1" "$2"
     fi
 }
@@ -23,9 +23,8 @@ echo "Thanks for installing fudesign2008.vim"
 echo "backing up current vim config"
 today=$(date +%Y%m%d)
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
-    [ -e "$i" ] && [ ! -L "$i" ] && mv "$i" "$i.$today";
+    [ -e "$i" ] && [ ! -L "$i" ] && mv "$i" "$i.$today"
 done
-
 
 if [ ! -e "$endpath/.git" ]; then
     echo "cloning fudesign2008.vim"
@@ -35,7 +34,6 @@ else
     cd "$endpath" && git pull && git checkout fuyg/plug
 fi
 
-
 echo "setting up symlinks"
 lnif "$endpath/.vimrc" "$HOME/.vimrc"
 lnif "$endpath/.vimrc.fork" "$HOME/.vimrc.fork"
@@ -44,24 +42,15 @@ lnif "$endpath/.vimrc.bundles.fork" "$HOME/.vimrc.bundles.fork"
 lnif "$endpath/.vim" "$HOME/.vim"
 lnif "$endpath/coc-settings.json" "$HOME/.vim/coc-settings.json"
 
-
-
-
-
 if [ ! -d "$endpath/.vim/bundle" ]; then
     mkdir -p "$endpath/.vim/bundle"
 fi
 
-
-
-if [ ! -e "$endpath/.vim/autoload/plug.vim" ];then
+if [ ! -e "$endpath/.vim/autoload/plug.vim" ]; then
     echo "Install Plug"
     curl -fLo "$endpath/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-
 echo "update/install plugins using Plug"
 # vim -es -u "$endpath/.vimrc.bundles" -i NONE -c "PlugInstall" -c "qa"
 vim -u "$endpath/.vimrc.bundles" +PlugUpdate! +PlugClean +qall
-
-
